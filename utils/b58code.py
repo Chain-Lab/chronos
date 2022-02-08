a44f91446f17abb70236c6bb79f0bc574175da1c
Decision <787362202@qq.com>
2022-02-08 22:30:12 +0800

    docs: add notes

--- utils/b58code.py
+++ utils/b58code.py
@@ -2,7 +2,7 @@ from hashlib import sha256
 from decorators import scrub_base58_input
 
 
-class b58code(object):
+class Base58Code(object):
 
     alphabet = b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
@@ -23,64 +23,92 @@ class b58code(object):
 
     # 对整数进行base58编码和解码
     @staticmethod
-    def encode_int(i, default_one=True):
+    def encode_int(i: int, default_one: bool = True):
+        """
+        :param i: int型, 需要进行编码的整数
+        :param default_one: bool型, 是否需要进行编码
+        :return: 返回编码后得到的bytes类型编码
+        """
         if not i and default_one:
-            return b58code.alphabet[0: 1]
+            return Base58Code.alphabet[0: 1]
         string = b""
         while i:
             i, idx = divmod(i, 58)
-            string = b58code.alphabet[idx: idx + 1] + string
+            string = Base58Code.alphabet[idx: idx + 1] + string
         return string
 
     @staticmethod
     @scrub_base58_input
-    def decode_int(v):
+    def decode_int(v: bytes):
+        """
+        :param v: bytes型, 传入需要解码的数据
+        :return: int型, 返回编码后得到的int型数据
+        """
         dec = 0
         for c in v:
-            dec = dec * 58 + b58code.alphabet.index(c)
+            dec = dec * 58 + Base58Code.alphabet.index(c)
         return dec
 
     # 对字符串进行base58编码和解码
     @staticmethod
     @scrub_base58_input
-    def encode(v):
+    def encode(v: bytes):
+        """
+        :param v: bytes型, 传入前经过装饰器进行数据类型确认
+        :return: bytes型, 编码后得到的数据
+        """
         nPad = len(v)
         v = v.lstrip(b'\0')
         nPad -= len(v)
 
         p, acc = 1, 0
-        for c in b58code.iseq(reversed(v)):
+        for c in Base58Code.iseq(reversed(v)):
             acc += p * c
             p = p << 8
 
-        result = b58code.encode_int(acc, default_one=False)
-        return b58code.alphabet[0: 1] * nPad + result
+        result = Base58Code.encode_int(acc, default_one=False)
+        return Base58Code.alphabet[0: 1] * nPad + result
 
     @staticmethod
     @scrub_base58_input
-    def decode(v):
+    def decode(v: bytes):
+        """
+        :param v: bytes型, 经过修饰器进行了类型转换
+        :return: bytes型, 返回解码的数据
+        """
         origin_len = len(v)
-        v = v.lstrip(b58code.alphabet[0: 1])
+        v = v.lstrip(Base58Code.alphabet[0: 1])
         new_len = len(v)
 
-        acc = b58code.decode_int(v)
+        acc = Base58Code.decode_int(v)
 
         result = []
         while acc > 0:
             acc, mod = divmod(acc, 256)
             result.append(mod)
 
-        return b'\0' * (origin_len - new_len) + b58code.bseq(reversed(result))
+        return b'\0' * (origin_len - new_len) + Base58Code.bseq(reversed(result))
 
     # base58check
     @staticmethod
-    def encode_check(v):
+    @scrub_base58_input
+    def encode_check(v: bytes):
+        """
+        带有数据校验的base58编码, 用于检查输入是否存在错误
+        :param v: bytes型, 经过了修饰器进行数据检查
+        :return: bytes型, 输出的编码数据
+        """
         digest = sha256(sha256(v).digest()).digest()
-        return b58code.encode(v + digest[:4])
+        return Base58Code.encode(v + digest[:4])
 
     @staticmethod
-    def decode_check(v):
-        result = b58code.decode(v)
+    def decode_check(v: bytes):
+        """
+        带有数据校验的base58解码, 可以确认编码是否存在错误
+        :param v: bytes型, 经过修饰器进行数据检查
+        :return: bytes型, 输出的解码数据
+        """
+        result = Base58Code.decode(v)
         result, check = result[:-4], result[-4:]
         digest = sha256(sha256(result).digest()).digest()
 
@@ -90,4 +118,4 @@ class b58code(object):
         return result
 
 if __name__ == "__main__":
-    print(b58code.encode("2334234234"))
+    print(Base58Code.encode("2334234234"))
--- utils/decorators.py
+++ utils/decorators.py
@@ -2,7 +2,15 @@
 
 # 装饰器，用于检验输入到base58部分函数的值是str或bytes类型
 def scrub_base58_input(func):
+    """
+    :param func: 用于装饰的函数，要求输入的数据为str或bytes
+    :return: 经过函数处理后返回的数据
+    """
     def wrapper(value):
+        """
+        :param value: 输入数据 (类型为str 或 bytes)
+        :return: 编码/解码后的数据
+        """
         # isinstance: 判断是否是已知的某种类型
         if isinstance(value, str) and not isinstance(value, bytes):
             value = value.encode("ascii")
