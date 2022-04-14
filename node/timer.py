9b6f28f3e579f2645d30d91d420074deb3f2010e
Decision <personal@decision01.com>
2022-04-14 15:28:15 +0800

    update: Interval time

--- node/timer.py
+++ node/timer.py
@@ -9,9 +9,8 @@ class Timer(Singleton):
     """
     节点的计时器， 在间隔时间后触发共识逻辑
     """
-    INTERVAL = 30
-    FINISH_INTERVAL = 10
-    # 便于调试，先设置为30秒
+    INTERVAL = 10
+    FINISH_INTERVAL = 5
 
     def __init__(self):
         self.__next_time = -1
