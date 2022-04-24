311e13fc410b7b355f9170602bb59e56dafca762
Decision <personal@decision01.com>
2022-04-24 14:57:03 +0800

    feat: increase timer interval

--- node/server.py
+++ node/server.py
@@ -221,7 +221,7 @@ class Server(object):
         # 投票信息同步完成
         # todo： 逻辑修改， 在到达时间点后直接进行处理
         # 这里的逻辑实际是存在问题的， server和每个client都有建立连接， 但是只和一个client判断信息同步完成， 并且每一次都要判断
-        if self.check_vote_synced(vote_data):
+        if Timer().finish() or self.check_vote_synced(vote_data):
             a = sorted(VoteCenter().vote.items(), key=lambda x: (x[1][-1], x[0]), reverse=True)
             # 如果同步完成， 按照第一关键字为投票数，第二关键字为地址字典序来进行排序
             # x的结构： addr1: [addr2 , addr3, ..., count]
