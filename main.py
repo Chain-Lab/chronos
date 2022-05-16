2dc5a98348ae49c12949250dc494ece4b19a9c33
Decision <personal@decision01.com>
2022-05-16 14:00:32 +0800

    feat: get block_info

--- main.py
+++ main.py
@@ -131,5 +131,37 @@ def clear():
     db.delete('block_chain1')
 
 
+def get_tx_data(height):
+    bc = BlockChain()
+    heights = []
+    result = []
+
+    for i in range(height):
+        print(i)
+        block = bc.get_block_by_height(i)
+        heights.append(i)
+        result.append(len(block.transactions))
+
+    print("heights: ", heights)
+    print("result:", result)
+
+
+def get_timestamp(height):
+    bc = BlockChain()
+    heights = []
+    result = []
+
+    block = bc.get_block_by_height(0)
+    timestamp = int(block.block_header.timestamp)
+
+    for i in range(1, height, 1):
+        block = bc.get_block_by_height(i)
+        heights.append(i)
+        result.append((int(block.block_header.timestamp) - timestamp) / 1000)
+        timestamp = int(block.block_header.timestamp)
+
+    print("heights: ", height)
+    print("result:", result)
+
 if __name__ == "__main__":
     fire.Fire()
